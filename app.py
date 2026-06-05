7c1a39e5155a7e6b2c9d0a9d156c19d8c49819a2
sheshanknaik23 <sheshanknaik77@gmail.com>
2026-06-05 14:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import warnings, shutil, tempfile, os
+import requests
+import io
 warnings.filterwarnings("ignore")
 
 # ── employee_type mapping ──
@@ -108,13 +110,15 @@ def msort(m):
         return int(p[1])*100 + MO.get(p[0][:3], 0)
     except: return 0
 
-FILE_PATH = "Exit & Exit Pip.xlsx"
+FILE_PATH = "https://j2w-my.sharepoint.com/:x:/g/personal/sheshank_suresh_joulestowatts_com/IQAbN1Juu0wxQaQeQrs_ALpaAf5cbpmso_hp1POy6u9adds?download=1"
 
 @st.cache_data(ttl=300)
 def load_data(path):
-    tmp = os.path.join(tempfile.gettempdir(), "exit_dash_tmp.xlsx")
-    shutil.copy2(path, tmp)
-    xl = pd.ExcelFile(tmp)
+    response = requests.get(path)
+    response.raise_for_status()
+
+    excel_data = io.BytesIO(response.content)
+    xl = pd.ExcelFile(excel_data)
 
     exit_df = xl.parse("Exit")
     pipe_df = xl.parse("Exit Pipeline")
